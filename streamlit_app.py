838fc4b7cd64ff94573af853f90efcd492b1d4e9
lynnlopez <lynnlopez66@gmail.com>
2024-07-16 14:20:01 -0500

    Update streamlit_app.py
    more changes to try to fix errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,29 +13,28 @@ st.write("The name on your Smoothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
-ingredients_list = st.multiselect(
-    "Choose up to 5 ingredients"
-    , my_dataframe
-    , max_selections=5
-    )
+#ingredients_list = st.multiselect(
+#    "Choose up to 5 ingredients"
+#    , my_dataframe
+#    , max_selections=5
+#    )
 
-if ingredients_list:
-    ingredients_string = ''
+#if ingredients_list:
+#    ingredients_string = ''
 
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen
+#    for fruit_chosen in ingredients_list:
+#        ingredients_string += fruit_chosen
 
-    #st.write(ingredients_string)
+#    #st.write(ingredients_string)
     
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
+#    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+#            values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
-    time_to_insert = st.button('Submit Order')
+#    time_to_insert = st.button('Submit Order')
 
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
+#    if time_to_insert:
+#        session.sql(my_insert_stmt).collect()
         
-        st.success('Your Smoothie is ordered, ' +  name_on_order + '!', icon="✅")
+#        st.success('Your Smoothie is ordered, ' +  name_on_order + '!', icon="✅")
