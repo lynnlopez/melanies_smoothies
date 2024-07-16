ac0b5f476c5774783f3963039b77ee3c8fd5cae8
lynnlopez <lynnlopez66@gmail.com>
2024-07-16 15:16:43 -0500

    Update streamlit_app.py
    added json to fruityvice call

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
-
 # Write directly to the app
 st.title("Customize Your Smoothie :cup_with_straw:")
 st.write(
@@ -40,3 +36,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered, ' +  name_on_order + '!', icon="✅")
+
+#New section to display fruityvice nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response.json())
